40ebf3389e634e76d133f22e5df5016788eb0bf5
Md. Mehedi Hasan <mehedi15-9021@diu.edu.bd>
2025-06-11 22:22:53 +0600

    first commit

--- src/main/java/org/mocion/app/pages/LoginPage.java
+++ src/main/java/org/mocion/app/pages/LoginPage.java
@@ -1,6 +1,9 @@
 package org.mocion.app.pages;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 
 public class LoginPage extends BasePage {
     private static final String LOGIN_SCREEN = "login_screen";
@@ -12,12 +15,14 @@ public class LoginPage extends BasePage {
     public LoginPage fillUserEmail() {
         click(LOGIN_SCREEN, "email_field");
         type(LOGIN_SCREEN, "email_field", "arifultester@maildrop.cc");
+        ((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.BACK));
         return this;
     }
 
     public LoginPage fillUserPassword() {
         click(LOGIN_SCREEN, "password_field");
         type(LOGIN_SCREEN, "password_field", "12345678");
+        ((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.BACK));
         return this;
     }
 
